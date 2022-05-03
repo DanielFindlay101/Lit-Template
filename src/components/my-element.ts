fbda7e376bd1832307ebe8c96aecc29776f734b1
DanielFindlay101 <danielfindlay95@gmail.com>
2022-05-03 13:53:27 +0100

    new commit

--- src/components/my-element.ts
+++ src/components/my-element.ts
@@ -1,24 +1,37 @@
 import {LitElement, html, css} from 'lit';
 import {customElement, property, query} from 'lit/decorators.js';
 
+type ToDoItem = {
+  text: string,
+  completed: boolean
+}
 @customElement("my-element")
 export class MyElement extends LitElement {
 
-    @property() 
+    @property({attribute: false})   //Don't understand the false attribute
     listItems = [
-      { text: "Make a list", completed: true },
+      { text: "Make a list", completed: false },
       { text: "Phone home" , completed: false }
     ]
 
-    @query('#newitem')
-    input!: HTMLInputElement
+    //styles
+    static styles = css`
+      .completed {
+        text-decoration-line: line-through;
+        color: #777;
+      }
+    `;
 
   render() {
     return html `
       <h2>ToDo</h2>
       <ul>
         ${this.listItems.map((item) => 
-          html `<li>${item.text}</li>`
+          html `<li
+          class=${item.completed ? 'completed' : ''}
+          @click=${() => this.toggleCompleted(item)}
+          >
+          ${item.text}</li>`
           )}
       </ul>
       <input id="newitem" placeholder="New item">
@@ -26,12 +39,19 @@ export class MyElement extends LitElement {
     `
   }
 
+  toggleCompleted(item: ToDoItem) {
+    item.completed = !item.completed;
+    this.requestUpdate()
+  }
+
+  @query('#newitem')
+  input!: HTMLInputElement
+
   addToDo() {
     this.listItems.push({ text: this.input.value, completed: false})
     this.input.value = '';
     this.requestUpdate();
-  }
-
+   }
   }
 
  
