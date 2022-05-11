5eb3ea764a48d605577f605bbb6770dc2dfcd582
DanielFindlay101 <danielfindlay95@gmail.com>
2022-05-11 16:47:35 +0100

    first commit

--- src/components/TestComponent.ts
+++ src/components/TestComponent.ts
@@ -1,11 +1,33 @@
-import {LitElement, html} from 'lit';
-import {customElement} from 'lit/decorators.js';
-
+import {LitElement, html, PropertyValues} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
 @customElement('my-element')
 class MyElement extends LitElement {
+  @property() forward = ''
+  @property() backward = ''
+
+  willUpdate(changedProperties: PropertyValues<this>) {
+    if(changedProperties.has('forward')) {
+      this.backward = this.forward.split('').reverse().join('')
+    }
+    if(changedProperties.has('backward')){
+      this.forward = this.backward.split('').reverse().join('') 
+    }
+  }
+  onInput(e: Event) {
+    const inputEl = e.target as HTMLInputElement
+    if(inputEl.id === 'forward'){
+      this.forward = inputEl.value
+    } else {
+      this.backward = inputEl.value
+    }
+  }
+
   render() {
     return html`
-      <div>Hello Everyone!!</div>
-    `;
+      <label>Forward: <input id="forward" @input=${this.onInput} .value=${this.forward}></label>
+      <label>Backward: <input id="backward" @input=${this.onInput} .value=${this.backward}></label>   
+      <div>Forward Text: ${this.forward}</div> 
+      <div>Backward Text: ${this.backward}</div> 
+    `
   }
 }
