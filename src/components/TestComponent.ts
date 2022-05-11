80f425e7f4c8770c8d74d4bfb24c93a919019ef9
DanielFindlay101 <danielfindlay95@gmail.com>
2022-05-11 10:18:42 +0100

    tried to implement JS timer

--- src/components/TestComponent.ts
+++ src/components/TestComponent.ts
@@ -1,11 +1,22 @@
-import {LitElement, html} from 'lit';
+import Timer from 'easytimer.js';
+import {LitElement, html, _$LE} from 'lit';
 import {customElement} from 'lit/decorators.js';
 
 @customElement('my-element')
 class MyElement extends LitElement {
-  render() {
-    return html`
-      <div>Hello Everyone!!</div>
+
+
+ render() {
+  const timer = new Timer();
+  timer.start();
+
+  timer.addEventListener('secondsUpdated', function(e) {
+    $('#basicUsage').html(timer.getTimeValues().toString());
+  })
+
+  return html`
+    <h1>My timer</h1>
+    <div id="basicUsage">00:00:00</div>
     `;
   }
 }
