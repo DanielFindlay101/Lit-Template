0f32ea1c4b00854d482840352d13091fe4a256f8
DanielFindlay101 <danielfindlay95@gmail.com>
2022-05-11 15:14:31 +0100

    first commit

--- src/components/TestComponent.ts
+++ src/components/TestComponent.ts
@@ -1,11 +1,34 @@
 import {LitElement, html} from 'lit';
-import {customElement} from 'lit/decorators.js';
-
+import {customElement, state} from 'lit/decorators.js';
+import {map} from 'lit/directives/map.js'
 @customElement('my-element')
 class MyElement extends LitElement {
+  @state()
+  things= [
+    "Raindrops on roses",
+    "Whiskers on kittens",
+    "Bright copper kettles",
+    "Warm woolen mittens"
+  ]
+  
   render() {
-    return html`
-      <div>Hello Everyone!!</div>
-    `;
+  return html`
+    <p>A few of my favourite things</p>
+   ${map (
+     this.things,
+     (thing, index) => html`
+      <li>
+        ${thing}
+        <button @click=${() => this._deleteThing(index)}/>
+          Delete
+        </button>
+      </li>
+      `
+    )}
+  `
+  }
+
+  private _deleteThing(index: number) {
+    this.things = this.things.filter((_, i) => i !== index)
   }
 }
