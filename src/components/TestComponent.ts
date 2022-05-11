897c34287eb1def672a5ee1f1dc286f1e10a7721
DanielFindlay101 <danielfindlay95@gmail.com>
2022-05-11 16:54:40 +0100

    first commit

--- src/components/TestComponent.ts
+++ src/components/TestComponent.ts
@@ -1,11 +1,43 @@
-import {LitElement, html} from 'lit';
-import {customElement} from 'lit/decorators.js';
+import {LitElement, html, css, PropertyValues} from 'lit';
+import {customElement, state, query} from 'lit/decorators.js';
 
 @customElement('my-element')
 class MyElement extends LitElement {
+  static styles = css`
+    :host {
+      display: block;
+    }
+    #message {
+      position: fixed;
+      background-color: cornflowerblue;
+      color: white;
+      padding: 10px;
+    }
+  `;
+  @state()
+  _showMessage = false
+  @query('#message')
+  _message!: HTMLDivElement
+
   render() {
     return html`
-      <div>Hello Everyone!!</div>
+      <button @click=${() => this._showMessage = !this._showMessage}>Click Me</button>
+      <div id = "message" ?hidden=${!this._showMessage}>
+          TADA
+      </div>
     `;
+  }
+  protected updated(changedProperties: PropertyValues<this>): void {
+    if (changedProperties.has('_showMessage')) {
+      const final = this._message.getBoundingClientRect().width;
+      const starting = 0 - final;
+      var player = this._message.animate([
+        { transform: `translateX(${starting}px)` },
+        { transform: `translateX(0)` }
+      ], {
+        duration: 500,
+        easing: 'ease-out',
+      });
+    }
   }
 }
