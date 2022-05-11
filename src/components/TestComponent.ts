66a2e50884c486112e68dc2ec8cba409112fe914
DanielFindlay101 <danielfindlay95@gmail.com>
2022-05-11 11:17:29 +0100

    first commit

--- src/components/TestComponent.ts
+++ src/components/TestComponent.ts
@@ -1,11 +1,18 @@
 import {LitElement, html} from 'lit';
-import {customElement} from 'lit/decorators.js';
+import {customElement, state} from 'lit/decorators.js';
+import { map } from 'lit/directives/map.js'
 
 @customElement('my-element')
 class MyElement extends LitElement {
+  @state()
+  guitars = new Set (["Ibanez", "Jackson", "Fender", "Gibson"])
+
   render() {
     return html`
-      <div>Hello Everyone!!</div>
-    `;
+    <p>My guitar list</p>
+    <ul>
+      ${map(this.guitars, (guitar) => html`<li>${guitar}</li>`)}
+    </ul>
+    `
   }
 }
