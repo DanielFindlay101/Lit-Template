1f0c4ccb78a36aaada8f6599fa03d6d86ab46306
DanielFindlay101 <danielfindlay95@gmail.com>
2022-05-12 15:06:57 +0100

    timer error again

--- src/components/TestComponent.ts
+++ src/components/TestComponent.ts
@@ -1,11 +1,30 @@
-import {LitElement, html} from 'lit';
-import {customElement} from 'lit/decorators.js';
+import { html, LitElement } from 'lit';
+import { customElement, property, state } from 'lit/decorators.js';
 
-@customElement('my-element')
-class MyElement extends LitElement {
-  render() {
-    return html`
-      <div>Hello Everyone!!</div>
-    `;
+@customElement('word-viewer')
+class WordViewer extends LitElement {
+  @state() private idx = 0;
+  @property() words = 'initial value';
+
+  private intervalTimer?: number;
+  
+  connectedCallback() {
+    super.connectedCallback();
+    this.intervalTimer = setInterval(this.tickToNextWord, 1000);
   }
+  
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    clearInterval(this.intervalTimer);
+    this.intervalTimer = undefined;
+  }
+
+  render() {
+    const splitWords = this.words.split('.');
+    const word = splitWords[this.idx % splitWords.length];
+    return html`<pre>${word}</pre>`;
+  }
+
+  tickToNextWord = () => { this.idx += 1; };
 }
+
