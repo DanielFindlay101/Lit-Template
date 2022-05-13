0f635fd2d5caca3d65d3d88ad652b6be4132d642
DanielFindlay101 <danielfindlay95@gmail.com>
2022-05-13 10:18:13 +0100

    first commit

--- src/components/TestComponent.ts
+++ src/components/TestComponent.ts
@@ -1,11 +1,27 @@
 import {LitElement, html} from 'lit';
-import {customElement} from 'lit/decorators.js';
+import {customElement, property} from 'lit/decorators.js';
+import {styles} from './styles'
 
-@customElement('my-element')
-class MyElement extends LitElement {
-  render() {
+@customElement('motion-carousel')
+class MotionCarousel extends LitElement {
+  static styles = styles;
+
+  private selectdInternal = 0
+  @property({type: Number})
+  selected = 0;
+
+  get maxSelected() {
+    return this.childElementCount - 1
+  }
+
+  hasValidSelected() {
+    return this.selected >=0
+  }
+
+  render() {  
     return html`
-      <div>Hello Everyone!!</div>
-    `;
+    <div class="fit">
+      <slot></slot>
+    </div>`
   }
 }
