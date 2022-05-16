8a7f4e1aa54e66cd62cdd3c8a2998a2c9485b419
DanielFindlay101 <danielfindlay95@gmail.com>
2022-05-16 14:55:36 +0100

    included screen shot capability

--- src/components/TestComponent.ts
+++ src/components/TestComponent.ts
@@ -6,13 +6,13 @@ import {customElement, query, property} from 'lit/decorators.js';
 export class MyElement extends LitElement {
 
  @query('#canvas')
- canvas: HTMLCanvasElement
+ canvas!: HTMLCanvasElement
 
  @query('#video')
  video: HTMLVideoElement
 
  render() {
-  // const ctx = this.canvas.getContext('2d')
+  
   if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices.getUserMedia({video: true}).then(stream => {
       this.video.srcObject = stream
@@ -20,10 +20,14 @@ export class MyElement extends LitElement {
     })
   }
    return html`
-      <h1>Testing</h1>
       <video id="video" width="640px" height="480px" autoplay></video>
-      <button id="snap">Take Photo</button>
+      <button id="snap" @click=${() => this._takePhoto()}>Take Photo</button>
       <canvas id="canvas" width="640px" height="480px"></canvas>
     `
+   }
+
+   private _takePhoto() {
+    const ctx = this.canvas.getContext('2d')
+    ctx?.drawImage(this.video, 0, 0, 640, 480)
    }
   }
