da1add1f02d3945e4a566330d0e23b4384b341b4
DanielFindlay101 <danielfindlay95@gmail.com>
2022-05-16 14:39:55 +0100

    working video component

--- src/components/TestComponent.ts
+++ src/components/TestComponent.ts
@@ -1,15 +1,29 @@
 import {LitElement, html} from 'lit';
-import {customElement} from 'lit/decorators.js';
-import {ClockController} from './clock-controller'
+import {customElement, query, property} from 'lit/decorators.js';
+
 
 @customElement('my-element')
-class MyElement extends LitElement {
-  // Create the controller and store it
-  private clock = new ClockController(this, 100);
+export class MyElement extends LitElement {
 
-  // Use the controller in render()
-  render() {
-    const formattedTime =(this.clock.value);
-    return html`${formattedTime}`;
+ @query('#canvas')
+ canvas: HTMLCanvasElement
+
+ @query('#video')
+ video: HTMLVideoElement
+
+ render() {
+  // const ctx = this.canvas.getContext('2d')
+  if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({video: true}).then(stream => {
+      this.video.srcObject = stream
+      this.video.play()
+    })
   }
-}
+   return html`
+      <h1>Testing</h1>
+      <video id="video" width="640px" height="480px" autoplay></video>
+      <button id="snap">Take Photo</button>
+      <canvas id="canvas" width="640px" height="480px"></canvas>
+    `
+   }
+  }
